d0da513f036084f182ff833779406e3b418375a6
Totoro <1131225133@qq.com>
2017-11-12 09:57:53 +0800

    coding

--- sample/src/main/java/com/totoro/xkf/sample/MainActivity.java
+++ sample/src/main/java/com/totoro/xkf/sample/MainActivity.java
@@ -15,12 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //组件必须设置的值为eageCount，也就是边数，必须大于3
+        //loopCount，有几层，必须大于0
+        //每个方向的值
         PolygonView polygonView = findViewById(R.id.pv_polygon_view);
+        //设置每个方向的值
+        //如果不设置的话就不会绘制
         List<Float> pointValue = new ArrayList<>();
+        //每个方向值的大小不能超过1，在是那个方向的整体比例，请计算好在设置
+        //pointValue的size一定要大于边数
         for (int i = 0; i < polygonView.getEageCount(); i++) {
             pointValue.add((float) (Math.random() * 1));
         }
         polygonView.setPointValue(pointValue);
+        //每个方向的的文字，可以不设置
         List<String> pointName = new ArrayList<>();
         pointName.add("Activity");
         pointName.add("BroadcastReceiver");
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         pointName.add("View");
         pointName.add("Handle");
         polygonView.setPointName(pointName);
+        //如果不调用这个方法的话，组件可以正常绘制，但是没有动画效果
         polygonView.draw();
     }
 }
